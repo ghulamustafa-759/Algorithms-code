409c0f102298af5be915c4fb2f68ec8ac3a15f61
ghulamustafa-759 <ghulammustafa759@gmail.com>
2024-10-05 14:57:26 +0500

    insertion sort

---
+++ Recursion Algos/insertion-sort.py
@@ -0,0 +1,15 @@
+def inser_sort(A):
+    for j in range(1, len(A)):
+        key = A[j]
+        i = j - 1
+        while i >= 0 and A[i] > key:
+            A[i+1] = A[i]
+            i -= 1
+        A[i+1] = key
+    
+    return A 
+
+
+
+A = [5,2,4,1,6,3]
+print(inser_sort(A))
