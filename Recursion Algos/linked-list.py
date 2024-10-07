91c6ecef0ac24dce7de73a44a999e17afa5119c5
ghulamustafa-759 <ghulammustafa759@gmail.com>
2024-10-07 11:36:55 +0500

    adding functions insert, search, delete node, delete tail

--- Recursion Algos/linked-list.py
+++ Recursion Algos/linked-list.py
@@ -18,7 +18,34 @@ class LinkedList:
                   print(first.data)
                   first = first.next
 
+        def insert_new_header(self, new_data):
+            new_node = Node(new_data)
+            new_node.next = self.head
+            self.head = new_node
 
+        def search(self,x):
+            temp = self.head
+            while temp is not None:
+                if temp.data == x:
+                    return True
+                temp = temp.next
+            else:
+                return False
+
+        def delete_node(self, data):
+            temp = self.head
+            while temp is not None:
+                if temp.data == data:
+                    break
+                prev = temp
+                temp = temp.next
+                prev.next = temp.next
+
+        def delete_tail(self):
+            temp = self.head
+            while temp.next.next is not None:
+                temp = temp.next
+            temp.next = None
 
 family = LinkedList()
 family.head = Node("Bob")
@@ -30,4 +57,15 @@ family.head.next = wife
 wife.next = first_kid
 first_kid.next = second_kid
 
+#family.traversal()
+family.insert_new_header("Dave")
 family.traversal()
+print("\n")
+
+family.delete_node("Bob")
+family.traversal()
+print("\n")
+
+family.delete_tail()
+family.traversal()
+#inserting elements
