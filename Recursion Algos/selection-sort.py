e1225fce4e74cbdd1d3c963e9acbdd3332cb51ab
ghulamustafa-759 <ghulammustafa759@gmail.com>
2024-10-20 16:49:59 +0500

    update to selection sort

--- Recursion Algos/selection-sort.py
+++ Recursion Algos/selection-sort.py
@@ -1,7 +1,14 @@
-def selection_sort(arr):
-    for i in range(len(arr)):
-        if arr[i]>arr[i]+1:
-            arr[i+1], arr[i]  = arr[i], arr[i+1]
-            print(arr)
 
-selection_sort([2,5,3,6,34])
+def selection_sort(arr):
+    for i in range(0,len(arr)-1): #last will be sorted automatically
+        cur_min = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[cur_min]:
+                cur_min = j
+        arr[i] , arr[cur_min] = arr[cur_min], arr[i] 
+    
+
+arr = [2,10,34,22,4,3]
+
+selection_sort(arr)
+print(arr)
